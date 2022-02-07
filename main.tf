c53ce9c893fe788515ec8e719aa1c7a642d895de
chrishamillsnyk <88782898+chrishamillsnyk@users.noreply.github.com>
2022-02-07 21:40:15 +0000

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,40 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-west-2"
+}
+
+module "s3_bucket" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+
+  bucket = "ch2022-s3-bucket"
+  acl    = "private"
+
+  versioning = {
+    enabled = true
+  }
+
+}
+
+module "s3_bucket2" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+
+  bucket = "ch2022-s3-bucket-new"
+  acl    = "private"
+
+  versioning = {
+    enabled = false
+  }
+
+}
+
